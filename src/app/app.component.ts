a271d934c5c303f591513dbc7977518011e5a51b
unknown <alextello1@hotmail.com>
2020-05-21 13:53:19 -0600

    Estructura base de angular que escuche y emite eventos a sockets
    readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'SocketsConAngular';
+  constructor(
+    public wService: WebsocketService,
+    public chatService: ChatService
+  ) {
+    this.wService.checkStatus();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { environment } from './../environments/environment';
+
+const config: SocketIoConfig = { url: environment.url, options: {} };
 
 import { AppComponent } from './app.component';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FooterComponent, ChatComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config), FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
